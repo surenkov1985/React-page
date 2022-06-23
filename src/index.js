82752f57284d4a957653209e0a0e4f148c897437
surenkov1985 <surenkov1985@gmail.com>
2022-06-23 21:28:23 +0300

    верстка без адаптива

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import "./assets/scss/style.scss"
+import React from "react";
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+import CardTitle from "./assets/components/cardTitle";
+import CardForm from "./assets/components/cardForm"
+import CardTooltip from "./assets/components/cardTooltip";
+
+
+root.render(
+	<div className="card">
+		<div className="card__container">
+			<CardTitle/>
+			<CardForm/>
+
+		</div>
+	</div>,
+
+
+	container
+);
